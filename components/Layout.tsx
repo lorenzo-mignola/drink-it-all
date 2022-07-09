43131dc1007e2ee9984006ae7c9d6cf0985f621d
lorenzo-mignola <lo.mignola@gmail.com>
2022-07-09 11:24:52 +0200

    Add style to header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,19 @@
 /** @jsx h */
 import { h, ComponentChildren, Fragment } from 'preact';
 import { tw } from '@twind';
+import { css } from 'twind/css';
 
 interface LayoutProps {
   children: ComponentChildren;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const customStyle = tw(css({ color: 'white', fontWeight: 'bold' }));
+
   return (
     <Fragment>
       <div className={tw`bg-primary-dark p-4`}>
-        <h1
-          className={tw`text(3xl) border(b-2 accent) inline`}
-          style={{ color: 'white', fontWeight: 'bold' }}
-        >
+        <h1 className={tw`text(3xl) border(b-2 accent) inline ${customStyle}`}>
           Header
         </h1>
       </div>
